c23fb722c5dd1efb973bec6fde60a5e8a019ae72
SONCHUNG <99730398+SONCHUNG@users.noreply.github.com>
2023-03-30 10:36:03 +0900

    [refactor]

--- src/main/java/com/modeul/web/entity/StuffView.java
+++ src/main/java/com/modeul/web/entity/StuffView.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-// 상속해서 Constructor를 만들기 위해서는 부모 Entity에서도 기본 생성자가 필요하다.
 
 @ToString
 @Getter
@@ -17,7 +16,5 @@ import lombok.Setter;
 public class StuffView extends Stuff {
 	private Long memberId;
 	private long categoryId;
-	
-	// 카테고리 이름이 필요하네..
 	private String categoryName;
 }
