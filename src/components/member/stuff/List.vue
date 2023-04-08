e7ccc45460e2a950564ddce352b6ad5c1a73f1ec
sungmin <kimmoksil@gmail.com>
2023-04-08 18:12:41 +0900

    [feat] 리스트 더보기 기능 추가

--- src/components/member/stuff/List.vue
+++ src/components/member/stuff/List.vue
@@ -1,122 +1,143 @@
+<script>
+
+export default {
+	data() {
+		return {
+			list: '',
+			page: ''
+		};
+	},
+	methods: {
+		addListHandler() {
+			this.page++;
+			fetch(`http://localhost:8080/member/stuffs?p=${this.page}`)
+				.then(response => response.json())
+				.then(list => {
+					this.list = list;
+					console.log(list);
+				})
+				.catch(error => console.log('error', error));
+		}
+	},
+	mounted() {
+		this.page = 0;
+		this.addListHandler();
+		console.log(this.list);
+	}
+}
+</script>
 <template>
-            <section class="canvas p-rel b-rad-2">
-            <header class="d-fl-al header-jc">
+	<section class="canvas p-rel b-rad-2">
+		<header class="d-fl-al header-jc">
+			<select class="selectbox-set" onchange="if(this.value) location.href=(this.value);">
+				<option value="">신수동</option>
+				<option value="">신설동</option>
+				<option value="/member/stuff/gps">GPS설정</option>
+			</select>
 
-                <select class="selectbox-set" onchange="if(this.value) location.href=(this.value);">
-                    <option value="">신수동</option>
-                    <option value="">신설동</option>
-                    <option value="/member/stuff/gps">GPS설정</option>
-                </select>
+			<div>
+				<!-- <a class="icon icon-menu">메뉴</a> -->
+				<a class="icon icon-alarm">알림</a>
+				<a class="icon">
+					<input type="checkbox" id="menuicon">
+					<label for="menuicon">
+						<span></span>
+						<span></span>
+						<span></span>
+					</label>
+					<div class="sidebar">
+						<div class="welcome f-weight">
+							<span class="f-color-2">뉴렉님</span><br>환영합니다.
+						</div>
+						<div class="side-menu">
+							<div></div>
+							<span class="sidebar-padding"><router-link to="/member/stuff/list">홈</router-link></span>
+							<span class="sidebar-padding"><router-link to="/member/stuff/listsearch">검색하기</router-link></span>
+							<span class="sidebar-padding"><router-link to="/member/stuff/reg">글 등록하기</router-link></span>
+							<span class="sidebar-padding"><router-link to="/member/participation/list">채팅하기</router-link></span>
+						</div>
+					</div>
+				</a>
+			</div>
+		</header>
 
-                <div>
-                    <!-- <a class="icon icon-menu">메뉴</a> -->
-                    <a class="icon icon-alarm">알림</a>
-                    <a class="icon">
-                        <input type="checkbox" id="menuicon">
-                        <label for="menuicon">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </label>
-                        <div class="sidebar">
-                            <div class="welcome f-weight">
-                                <span class="f-color-2">뉴렉님</span><br>환영합니다.
-                            </div>
-                            <div class="side-menu">
-                                <div></div>
-                                <span class="sidebar-padding"><router-link to="/member/stuff/list">홈</router-link></span>
-                                <span class="sidebar-padding"><router-link to="/member/stuff/listsearch">검색하기</router-link></span>
-                                <span class="sidebar-padding"><router-link to="/member/stuff/reg">글 등록하기</router-link></span>
-                                <span class="sidebar-padding"><router-link to="/member/participation/list">채팅하기</router-link></span>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </header>
-            
- 			<nav>
-                  <form action="list.html" method="get" class="header-categ-box">
-                  	 <div> 
-                  	 	<button class="header-categ" name="c" value="1">전체</button>
-                  	 </div>
-                  	 	
-			         <div>
-			            <button class="header-categ" name="c" value="1">일반상품</button>
-			            <button class="header-categ" name="c" value="2">딜리버리 푸드</button>
-            			<button class="header-categ" name="c" value="3">대형마트 대량 물품</button>
-			         </div>
-			      </form>
-			</nav>
+		<nav>
+			<form action="list.html" method="get" class="header-categ-box">
+				<div>
+					<button class="header-categ" name="c" value="1">전체</button>
+				</div>
 
-            <!-- 나중에 onclick 이벤트 하트 부분만 빼고 넣기 -->
-            <main>
-                <div class="d-gr li-gr m-t-13px list-cl" onclick="location.href='detail.html'">
-                    <!-- 나중에 전체를 div로 묶어서 main으로 크게 묶기 -->
-                    <div class="li-pic b-rad-1">사진</div>
-                    <div class="li-categ header-categ li-header-categ">딜리버리 푸드</div>
-                    <div class="li-heart icon icon-heart">
-                        찬하트
-                    </div>
-                    <div class="li-subj">서강대 앞 교촌치킨 시켜드실 분</div>
-                    <div class="li-member">2/5</div>
-                    <div class="li-date">2월 3일 금 18시까지</div>
-                </div>
-                <div>
-                    <h1 class="icon icon-line">선 긋기</h1>
-                </div>
+				<div>
+					<button class="header-categ" name="c" value="1">일반상품</button>
+					<button class="header-categ" name="c" value="2">딜리버리 푸드</button>
+					<button class="header-categ" name="c" value="3">대형마트 대량 물품</button>
+				</div>
+			</form>
+		</nav>
+		<!-- 나중에 onclick 이벤트 하트 부분만 빼고 넣기 -->
+		<main>
+			<div class="stuff-list" v-for="stuff in list" >
+				<div class="d-gr li-gr m-t-13px list-cl" onclick="location.href='detail.html'">
+					<!-- 나중에 전체를 div로 묶어서 main으로 크게 묶기 -->
+					<div class="li-pic b-rad-1">사진</div>
+					<div class="li-categ header-categ li-header-categ">{{ stuff.categoryName }}</div>
+					<div class="li-heart icon icon-heart">
+						찬하트
+					</div>
+					<div class="li-subj">{{ stuff.title }}</div>
+					<div class="li-member"> 1 / {{stuff.numPeople}}</div>
+					<div class="li-date">{{ stuff.deadline }}</div>
+				</div>
+				<div>
+					<h1 class="icon icon-line">선 긋기</h1>
+				</div>
+			</div>
 
-                <div class="d-gr li-gr  list-cl" onclick="location.href='detail.html'">
-                    <!-- 나중에 전체를 div로 묶어서 main으로 크게 묶기 -->
-                    <div class="li-pic b-rad-1">사진</div>
-                    <div class="li-categ header-categ li-header-categ">딜리버리 푸드</div>
-                    <div class="li-heart icon icon-heart">
-                        찬하트
-                    </div>
-                    <div class="li-subj">서강대 앞 교촌치킨 시켜드실 분</div>
-                    <div class="li-member">2/5</div>
-                    <div class="li-date">2월 3일 금 18시까지</div>
-                </div>
-                <div>
-                    <h1 class="icon icon-line">선 긋기</h1>
-                </div>
 
-                <div class="d-gr li-gr list-cl" onclick="location.href='detail.html'">
-                    <!-- 나중에 전체를 div로 묶어서 main으로 크게 묶기 -->
-                    <div class="li-pic b-rad-1">사진</div>
-                    <div class="li-categ header-categ li-header-categ">딜리버리 푸드</div>
-                    <div class="li-heart icon icon-heart">
-                        찬하트
-                    </div>
-                    <div class="li-subj">서강대 앞 교촌치킨 시켜드실 분</div>
-                    <div class="li-member">2/5</div>
-                    <div class="li-date">2월 3일 금 18시까지</div>
-                </div>
-                <div>
-                    <h1 class="icon icon-line">선 긋기</h1>
-                </div>
-            </main>
 
-            <nav class="navi-bar d-fl-jf" style="display: none;">
-                <div>
-                    <router-link class="icon icon-home m-notop" to="./list">home</router-link>
-                </div>
-                <div>
-                    <a class="icon icon-search m-notop" href="./list-search.html">search</a>
-                </div>
-                <div>
-                    <a class="icon icon-post m-notop" href="./reg.html">post+</a>
-                </div>
-                <div>
-                    <a class="icon icon-chat m-notop" href="../participation/list.html">chat</a>
-                </div>
-                <div>
-                    <a class="icon icon-info m-notop">mypage</a>
-                </div>
-            </nav>
-        </section>
+			<button class="btn-next more-list" @click="addListHandler">더보기</button>
+			<div class="reg-stuff">
+				+
+			</div>
+
+		</main>
+
+
+		<nav class="navi-bar d-fl-jf" style="display: none;">
+			<div>
+				<router-link class="icon icon-home m-notop" to="./list">home</router-link>
+			</div>
+			<div>
+				<a class="icon icon-search m-notop" href="./list-search.html">search</a>
+			</div>
+			<div>
+				<a class="icon icon-post m-notop" href="./reg.html">post+</a>
+			</div>
+			<div>
+				<a class="icon icon-chat m-notop" href="../participation/list.html">chat</a>
+			</div>
+			<div>
+				<a class="icon icon-info m-notop">mypage</a>
+			</div>
+		</nav>
+	</section>
 </template>
 
 
 <style scoped>
-    @import "/css/component/member/stuff/component-list.css";
+@import "/css/component/member/stuff/component-list.css";
+
+.reg-stuff {
+	width: 30px;
+	height: 30px;
+	background-color: #63A0C2;
+	color: #fff;
+	border-radius: 50%;
+	text-align: center;
+	line-height: 30px;
+	position: fixed;
+	right: 30px;
+	bottom: 30px;
+	cursor: pointer;
+}
 </style>
